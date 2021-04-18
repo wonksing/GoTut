2a06f9fbb641a5f01ea40023af6bcfbc2b34f8b7
“wonkdance” <wonkdance@gmail.com>
2021-04-19 07:12:48 +0900

    working on real world example

---
+++ auth/oauth2v4/example_realworld/oauth2-server/server.go
@@ -0,0 +1,335 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/go-oauth2/oauth2/v4/generates"
+	"github.com/spf13/viper"
+
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-oauth2/oauth2/v4/store"
+	"github.com/go-session/session"
+)
+
+var (
+	dumpvar bool
+	// idvar     string
+	// secretvar string
+	// domainvar string
+	// portvar   int
+
+	addr           string
+	configFileName string
+)
+
+func init() {
+	flag.StringVar(&addr, "addr", ":9096", "listening address(eg. :9096)")
+	flag.StringVar(&configFileName, "cn", "./configs/server.yml", "config file name")
+	flag.BoolVar(&dumpvar, "d", true, "Dump requests and responses")
+	// flag.StringVar(&idvar, "i", "12345", "The client id being passed in")
+	// flag.StringVar(&secretvar, "s", "12345678", "The client secret being passed in")
+	// flag.StringVar(&domainvar, "r", "http://localhost:9094", "The domain of the redirect url")
+	// flag.IntVar(&portvar, "p", 9096, "the base port for the server")
+}
+
+func main() {
+	flag.Parse()
+	if dumpvar {
+		log.Println("Dumping requests")
+	}
+
+	conf := viper.New()
+	conf.SetConfigFile(configFileName)
+	err := conf.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	authCodeAccessTokenExp := conf.GetInt("token_config.auth_code.access_token_exp")
+	authCodeRefreshTokenExp := conf.GetInt("token_config.auth_code.refresh_token_exp")
+	authCodeGenerateRefresh := conf.GetBool("token_config.auth_code.generate_refresh")
+	clientCredentialsAccessTokenExp := conf.GetInt("token_config.client_credential.access_token_exp")
+	clientCredentialsRefreshTokenExp := conf.GetInt("token_config.client_credential.refresh_token_exp")
+	clientCredentialsGenerateRefresh := conf.GetBool("token_config.client_credential.generate_refresh")
+
+	tokenStoreFilePath := conf.GetString("token_store.file.path")
+
+	clientStore := store.NewClientStore()
+	cc := conf.Sub("client_credentials")
+	ccSettings := cc.AllSettings()
+	for key, val := range ccSettings {
+		v := val.(map[string]interface{})
+		id := v["id"].(string)
+		secret := v["secret"].(string)
+		domain := v["domain"].(string)
+		fmt.Println(key)
+		clientStore.Set(id, &models.Client{
+			ID:     id,
+			Secret: secret,
+			Domain: domain,
+		})
+	}
+
+	manager := manage.NewDefaultManager()
+	// manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+	manager.SetAuthorizeCodeTokenCfg(&manage.Config{
+		AccessTokenExp:    time.Hour * time.Duration(authCodeAccessTokenExp),
+		RefreshTokenExp:   time.Hour * time.Duration(authCodeRefreshTokenExp),
+		IsGenerateRefresh: authCodeGenerateRefresh,
+	})
+	manager.SetClientTokenCfg(&manage.Config{
+		AccessTokenExp:    time.Hour * time.Duration(clientCredentialsAccessTokenExp),
+		RefreshTokenExp:   time.Hour * time.Duration(clientCredentialsRefreshTokenExp),
+		IsGenerateRefresh: clientCredentialsGenerateRefresh,
+	})
+
+	// token store
+	if tokenStoreFilePath != "" {
+		manager.MustTokenStorage(store.NewFileTokenStore(tokenStoreFilePath))
+	} else {
+		// memory
+		manager.MustTokenStorage(store.NewMemoryTokenStore())
+	}
+
+	// generate jwt access token
+	// manager.MapAccessGenerate(generates.NewJWTAccessGenerate("", []byte("00000000"), jwt.SigningMethodHS512))
+	manager.MapAccessGenerate(generates.NewAccessGenerate())
+
+	manager.MapClientStorage(clientStore)
+
+	// srv := server.NewServer(server.NewConfig(), manager)
+	srv := server.NewDefaultServer(manager)
+	srv.Config.AllowGetAccessRequest = true
+
+	// Password credentials
+	srv.SetPasswordAuthorizationHandler(func(username, password string) (userID string, err error) {
+		if username == "test" && password == "test" {
+			userID = "test"
+		}
+		return
+	})
+
+	// Authorization Code Grant
+	srv.SetUserAuthorizationHandler(userAuthorizeHandler)
+	// srv.SetUserAuthorizationHandler(userAuthorizeHandler2)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/auth", authHandler)
+
+	http.HandleFunc("/oauth/authorize", func(w http.ResponseWriter, r *http.Request) {
+		if dumpvar {
+			dumpRequest(os.Stdout, "authorize", r)
+		}
+
+		store, err := session.Start(r.Context(), w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var form url.Values
+		if v, ok := store.Get("ReturnUri"); ok {
+			form = v.(url.Values)
+		}
+		r.Form = form
+
+		fmt.Println("Form", r.Form)
+
+		store.Delete("ReturnUri")
+		store.Save()
+
+		err = srv.HandleAuthorizeRequest(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	})
+
+	// token request for all types of grant
+	// Client Credentials Grant comes here directly
+	http.HandleFunc("/oauth/token", func(w http.ResponseWriter, r *http.Request) {
+		if dumpvar {
+			_ = dumpRequest(os.Stdout, "/oauth/token", r) // Ignore the error
+		}
+
+		err := srv.HandleTokenRequest(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	// validate access token
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		if dumpvar {
+			_ = dumpRequest(os.Stdout, "test", r) // Ignore the error
+		}
+		token, err := srv.ValidationBearerToken(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		data := map[string]interface{}{
+			"expires_in": int64(token.GetAccessCreateAt().Add(token.GetAccessExpiresIn()).Sub(time.Now()).Seconds()),
+			"client_id":  token.GetClientID(),
+			"user_id":    token.GetUserID(),
+		}
+		e := json.NewEncoder(w)
+		e.SetIndent("", "  ")
+		e.Encode(data)
+	})
+
+	// client credential 저장
+	http.HandleFunc("/credentials", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "PUT" {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		clientID := r.FormValue("client_id")
+		clientSecret := r.FormValue("client_secret")
+		clientDomain := r.FormValue("client_domain")
+
+		err := clientStore.Set(clientID, &models.Client{
+			ID:     clientID,
+			Secret: clientSecret,
+			Domain: clientDomain,
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"CLIENT_ID": clientID, "CLIENT_SECRET": clientSecret})
+	})
+
+	log.Printf("Server is running at %v.\n", addr)
+	log.Printf("Point your OAuth client Auth endpoint to %s%s", "http://"+addr, "/oauth/authorize")
+	log.Printf("Point your OAuth client Token endpoint to %s%s", "http://"+addr, "/oauth/token")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v", addr), nil))
+}
+
+func dumpRequest(writer io.Writer, header string, r *http.Request) error {
+	data, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		return err
+	}
+	writer.Write([]byte("\n" + header + ": \n"))
+	writer.Write(data)
+	return nil
+}
+
+func userAuthorizeHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	if dumpvar {
+		_ = dumpRequest(os.Stdout, "userAuthorizeHandler", r) // Ignore the error
+	}
+	store, err := session.Start(r.Context(), w, r)
+	if err != nil {
+		return
+	}
+
+	uid, ok := store.Get("LoggedInUserID")
+	if !ok {
+		if r.Form == nil {
+			r.ParseForm()
+		}
+
+		store.Set("ReturnUri", r.Form)
+		store.Save()
+
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	userID = uid.(string)
+	store.Delete("LoggedInUserID")
+	store.Save()
+	return
+}
+
+func userAuthorizeHandler2(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	// 로그인
+
+	// 권한 허용
+
+	userID = "wonk"
+	return
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if dumpvar {
+		_ = dumpRequest(os.Stdout, "login", r) // Ignore the error
+	}
+	store, err := session.Start(r.Context(), w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if r.Method == "POST" {
+		if r.Form == nil {
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+		store.Set("LoggedInUserID", r.Form.Get("username"))
+		store.Save()
+
+		w.Header().Set("Location", "/auth")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	outputHTML(w, r, "static/login.html")
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	if dumpvar {
+		_ = dumpRequest(os.Stdout, "auth", r) // Ignore the error
+	}
+	store, err := session.Start(nil, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if _, ok := store.Get("LoggedInUserID"); !ok {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	outputHTML(w, r, "static/auth.html")
+}
+
+func outputHTML(w http.ResponseWriter, req *http.Request, filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer file.Close()
+	fi, _ := file.Stat()
+	http.ServeContent(w, req, file.Name(), fi.ModTime(), file)
+}
